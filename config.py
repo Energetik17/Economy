5910b3544f35f1d163ca59a7abd029c84c1ba1db
Energetik17 <kovalenkoaleksandr630@gmail.com>
2022-04-04 12:39:27 +0300

    Economy

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 settings = {
-	'TOKEN': '',
-	'NAME BOT': '',
-	'ID': 960422539358773298
+	'TOKEN': 'OTYwNDIyNTM5MzU4NzczMjk4.YkqNMQ.7mZSf-HRwW4qNvA13_ATQavOEN0',
+	'NAME BOT': 'Economy',
+	'ID': 960422539358773298,
 	'PREFIX': '!'
 	}
